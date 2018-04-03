1424ee96c4a5fcc5f967a75057861388a0b45ee2
Milosz Galazka <milosz@sleeplessbeastie.eu>
2018-04-03 22:27:52 +0000

    Created initial action

--- action.php
+++ action.php
@@ -1,33 +1,28 @@
 <?php
 /**
- * Example Action Plugin:   Example Component.
+ * unsplash action plugin: set background from unsplash
  *
- * @author     Samuele Tognini <samuele@cli.di.unipi.it>
+ * @author Milosz Galazka <milosz@sleeplessbeastie.eu>
  */
  
 if(!defined('DOKU_INC')) die();
  
  
-class action_plugin_example extends DokuWiki_Action_Plugin {
+class action_plugin_unsplash extends DokuWiki_Action_Plugin {
  
     /**
      * Register its handlers with the DokuWiki's event controller
      */
     public function register(Doku_Event_Handler $controller) {
-        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this,
-                                   '_hookjs');
+        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, '_addcss');
     }
  
     /**
-     * Hook js script into page headers.
-     *
-     * @author Samuele Tognini <samuele@cli.di.unipi.it>
+     * Add CSS into page headers.
      */
-    public function _hookjs(Doku_Event $event, $param) {
-        $event->data['script'][] = array(
-                            'type'    => 'text/javascript',
-                            'charset' => 'utf-8',
-                            '_data'   => '',
-                            'src'     => DOKU_PLUGIN.'example/example.js');
+    public function _addcss(Doku_Event $event, $param) {
+        $event->data['style'][] = array(
+                            'type'    => 'text/css',
+                            '_data'   => 'html {height: 100%;} body {min-height: 100%;background-image: url(https://source.unsplash.com/weekly/) !important; background-size: cover;background-blend-mode: screen; background-color: #aaa !important; }');
     }
 }
